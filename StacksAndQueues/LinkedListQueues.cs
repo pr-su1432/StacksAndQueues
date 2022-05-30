a00fa53aecc6cc6a400b4d265b50dfc565bf2a9d
[prasanna] <prasanna>
2022-05-30 21:13:24 +0530

    [prasanna]: adding elements in Queue

---
+++ StacksAndQueues/LinkedListQueues.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class LinkedListQueues
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+
+        internal void DisPlay()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine("{0} is in the top of the Queue", this.head.data);
+        }
+        
+    }
+}
